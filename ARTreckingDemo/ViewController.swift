350f95d4318ba10cdc3423a27585afc0d38cfb42
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-07-11 09:58:30 -0700

    Initial Commit

---
+++ ARTreckingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARTreckingDemo
+//
+//  Created by Arpit iOS Dev. on 11/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
